063b5173c9ad7bad0c44a1b41fca61116c01c3c0
Richard Webb <richard.webb@helpsystems.com>
2025-04-09 21:00:03 +0100

    wip

---
+++ src/Avalonia.FuncUI.TreeDataGrid/Library.fs
@@ -0,0 +1,22 @@
+﻿namespace Avalonia.FuncUI.DSL
+
+open Avalonia.Controls
+open Avalonia.FuncUI.Types
+open Avalonia.FuncUI.Builder
+
+[<AutoOpen>]
+module TreeDataGrid =
+
+    let create (attrs: IAttr<TreeDataGrid> list): IView<TreeDataGrid> =
+        ViewBuilder.Create<TreeDataGrid>(attrs)
+
+    type TreeDataGrid with
+
+        static member autoDragDropRows<'t when 't :> TreeDataGrid>(autoDragDropRows: bool) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<bool>(TreeDataGrid.AutoDragDropRowsProperty, autoDragDropRows, ValueNone)
+
+        static member canUserResizeColumns<'t when 't :> TreeDataGrid>(canUserResizeColumns: bool) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<bool>(TreeDataGrid.CanUserResizeColumnsProperty, canUserResizeColumns, ValueNone)
+
+        static member canUserSortColumns<'t when 't :> TreeDataGrid>(canUserSortColumns: bool) : IAttr<'t> =
+            AttrBuilder<'t>.CreateProperty<bool>(TreeDataGrid.CanUserSortColumnsProperty, canUserSortColumns, ValueNone)
